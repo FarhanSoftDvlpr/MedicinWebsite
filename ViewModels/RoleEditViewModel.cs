0b301c4d0801d54847763fd73b326d4d4c24e56f
FarhanSoftDvlpr <105435352+FarhanSoftDvlpr@users.noreply.github.com>
2026-05-17 22:49:47 +0530

    Add files via upload

---
+++ ViewModels/RoleEditViewModel.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace MEDICINE.WEB.Areas.Admin.ViewModels
+{
+    public class RoleEditViewModel
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [Display(Name = "Role Name")]
+        public string Name { get; set; }
+
+        [Display(Name = "Description")]
+        public string Description { get; set; }
+
+        public string RoleKey { get; set; }
+
+        public List<PermissionCheckboxViewModel> Permissions { get; set; }
+            = new List<PermissionCheckboxViewModel>();
+    }
+
+    public class PermissionCheckboxViewModel
+    {
+        public int PermissionId { get; set; }
+
+        public string PermissionName { get; set; }
+
+        public string PermissionKey { get; set; }
+
+        public string Category { get; set; }
+
+        public bool IsSelected { get; set; }
+    }
+}
